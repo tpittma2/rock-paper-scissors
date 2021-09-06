e1d1c9c09948589262ab5363f27170d3648671e4
TJ <tpittma2@gmail.com>
2021-09-06 17:59:21 -0400

    added rest of game logic

--- Scripts/rock-paper-scissors.js
+++ Scripts/rock-paper-scissors.js
@@ -1,16 +1,20 @@
 const playerBtnRock = document.querySelector('.player-btn-rock');
 const playerBtnPaper = document.querySelector('.player-btn-paper');
 const playerBtnScissors = document.querySelector('.player-btn-scissors');
-const playerScore = document.querySelector('.player-score');
+const playerScoreText = document.querySelector('.player-score');
 
-const computerBtnRock = document.querySelector('.player-btn-rock');
-const computerBtnPaper = document.querySelector('.player-btn-paper');
-const computerBtnScissors = document.querySelector('.player-btn-scissors');
-const computerScore = document.querySelector('.computer-score');
+const computerBtnRock = document.querySelector('.computer-btn-rock');
+const computerBtnPaper = document.querySelector('.computer-btn-paper');
+const computerBtnScissors = document.querySelector('.computer-btn-scissors');
+const computerScoreText = document.querySelector('.computer-score');
 
-const roundLog = document.querySelector('.round-log'); 
+const roundLog = document.querySelector('.round-log');
 
+const resultContainer = document.querySelector('.result-container');
 const playAgainBtn = document.querySelector('.play-again-button')
+const txtResultText = document.querySelector('.result-text');
+
+const HIDDEN_CLASS = 'hidden';
 
 /* 
 * Chooses a random number between 1-2 and uses that to return 'Rock', 'Paper', or 'Scissors'
@@ -79,39 +83,67 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
-function game(e) {
-    let playerScore = parseInt(playerScore.textContent);
-    let computerScore = 0;
-      let  result = playRound(e.target.dataset.button, computerPlay());
-
-        if (result.startsWith('You Win!')) {
-            playerScore++;
-        }
-        else if (result.startsWith('You Lose!')) {
-            computerScore++;
-        }
-        console.log(`${result} (Current Score: ${playerScore} to ${computerScore})`);
-
-    let finalScoreText;
-    if (playerScore > computerScore)
-        finalScoreText = 'You Win!';
-    else if (computerScore > playerScore)
-        finalScoreText = 'You Lose!';
-    else
-        finalScoreText = 'Tie Game!';
-    console.log();
-    console.log(`${finalScoreText} (Final Score: ${playerScore} to ${computerScore})`);
+function isGameOver() {
+    return !resultContainer.classList.contains(HIDDEN_CLASS)
 }
 
-function addLog(logText)
-{
+function setGameOver(resultText) {
+    txtResultText.textContent = resultText;
+    resultContainer.classList.remove(HIDDEN_CLASS);
+    let className = 'clickable-button';
+    playerBtnRock.classList.remove(className);
+    playerBtnPaper.classList.remove(className);
+    playerBtnScissors.classList.remove(className);
+    setActiveButtons();
+
+}
+
+function game(playerSelection) {
+    if (isGameOver())
+        return;
+
+    let computerSelection = computerPlay();
+    let playerScore = parseInt(playerScoreText.textContent);
+    let computerScore = parseInt(computerScoreText.textContent);
+    setActiveButtons(playerSelection, computerSelection);
+
+    let result = playRound(playerSelection, computerSelection);
+
+    if (result.startsWith('You Win!')) {
+        playerScore++;
+        playerScoreText.textContent = playerScore;
+    }
+    else if (result.startsWith('You Lose!')) {
+        computerScore++;
+        computerScoreText.textContent = computerScore;
+    }
+
+    addLog(result);
+    //console.log(`${result} (Current Score: ${playerScore} to ${computerScore})`);
+    if (playerScore == 5 || computerScore == 5) {
+        let finalScoreText;
+        if (playerScore > computerScore)
+            finalScoreText = 'You Win!';
+        else if (computerScore > playerScore)
+            finalScoreText = 'You Lose.';
+        else
+            finalScoreText = 'Tie Game!';
+        setGameOver(finalScoreText);
+
+
+    }
+    // console.log();
+    // console.log(`${finalScoreText} (Final Score: ${playerScore} to ${computerScore})`);
+}
+
+function addLog(logText) {
+    let roundNumber = roundLog.childNodes.length;
     const listItem = document.createElement('li');
-    listItem.textContent = logText;
-    roundLog.appendChild(listItem);
+    listItem.textContent = `Round ${roundNumber}: ${logText}`;
+    roundLog.insertBefore(listItem, roundLog.firstChild);
 }
 
-function clearActiveButtons()
-{
+function setActiveButtons(playerSelection, computerSelection) {
     let className = 'active-button';
     playerBtnRock.classList.remove(className);
     playerBtnPaper.classList.remove(className);
@@ -119,16 +151,52 @@ function clearActiveButtons()
     computerBtnRock.classList.remove(className);
     computerBtnPaper.classList.remove(className);
     computerBtnScissors.classList.remove(className);
+
+    let playerSelectedButton;
+    let computerSelectedButton;
+    switch (playerSelection.toLowerCase()) {
+        case 'rock':
+            playerSelectedButton = playerBtnRock;
+            break;
+        case 'paper':
+            playerSelectedButton = playerBtnPaper;
+            break;
+        case 'scissors':
+            playerSelectedButton = playerBtnScissors;
+            break;
+    }
+
+    switch (computerSelection.toLowerCase()) {
+        case 'rock':
+            computerSelectedButton = computerBtnRock;
+            break;
+        case 'paper':
+            computerSelectedButton = computerBtnPaper;
+            break;
+        case 'scissors':
+            computerSelectedButton = computerBtnScissors;
+            break;
+    }
+
+    if(playerSelectedButton != undefined)
+        playerSelectedButton.classList.toggle(className);
+    if(computerSelectedButton != undefined)
+        computerSelectedButton.classList.toggle(className);
+
 }
 
 function InitControls() {
 
     playAgainBtn.onclick = () => window.location.reload();
 
+    playerBtnRock.addEventListener('click', function (e) {
+        game(e.currentTarget.dataset.button);
+    });
+    playerBtnPaper.addEventListener('click', function (e) { game(e.currentTarget.dataset.button); });
+    playerBtnScissors.addEventListener('click', function (e) { game(e.currentTarget.dataset.button); });
 
 }
 InitControls();
-addLog("Test");
 //clearActiveButtons();
 
 //game();
